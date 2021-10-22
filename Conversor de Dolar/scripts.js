3c79f98358e5e88c8cd0d21482c13884ba318d7e
Welder Barroso de Melo <welderb40@gmail.com>
2021-10-22 13:31:30 -0300

    adding the files
    here i separete the html in css and js.

---
+++ Conversor de Dolar/scripts.js
@@ -0,0 +1,7 @@
+function calculaDolar() {
+    let real = document.getElementById("entrada-real").value
+    let dolar = document.getElementById("entrada-dolar").value
+
+    let resultado = real / dolar
+    alert(resultado)
+}
